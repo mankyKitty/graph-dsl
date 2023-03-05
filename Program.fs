ebf58900c4990bc96a45c17f7dd5a2f85117edd8
Sean Chalmers <sean.chalmers@obsidian.systems>
2023-03-05 11:48:51 +0000

    Flesh out handling of moves and tweak some types

--- Program.fs
+++ Program.fs
@@ -6,56 +6,107 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open Suave.RequestErrors
 
-type ElemSelect =
-    | GraphVertex
-    | GraphEdge
+open QuikGraph
+open GraphDSL.Zipper
+
+type Vert =
+    { Tag: string
+      Value: int }
+
+type AppGraph = BidirectionalGraph<Vert, TaggedEdge<Vert,string>>
 
 type Property =
     | Tag of string
     | Value of int
 
 type CompOp =
-    | EqualTo of ElemSelect * Property
+    | EqualTo of Property
     | ValueLessThan of int
     | ValueGreaterThan of int
 
+// Our boundary types are different to the internal zipper types so there isn't conceptual leakage between the two.
 type MoveOp =
-    | ToVertex of string    // Explicit tag name of the target vertex
+    | ToVertex of Vert // Explicit tag name of the target vertex
     | AlongEdge of string   // Explicit tag name of the edge to move along.
-    | FirstEdge of CompOp   // Move along the first edge matching the given selection options
+    | FirstEdge of string // Move along the first edge matching the given selection options
     | FirstVertex of CompOp // Try to move to the first vertex connected to the current vertex matching the given comparison
+    | Back
 
+let newEdge (a: Vert, b: Vert, t: string) : TaggedEdge<Vert, string> =
+    new TaggedEdge<Vert, string>(a,b,t)
 
-// Example for dumping JSON of structures
-// let main argv =
-//     let mv = ToVertex "camels"
-//     let s = Json.serialize mv
-//     printfn "%s" s
-//     0
+let newVert (tag: string, v: int): Vert =
+    { Tag = tag; Value = v }
 
-let f rq =
+let mkCompOp (c: CompOp): (Vert -> bool) =
+    match c with
+        | ValueLessThan i -> fun v -> v.Value < i
+        | ValueGreaterThan i -> fun v -> v.Value > i
+        | EqualTo (Tag s) -> fun v -> v.Tag = s
+        | EqualTo (Value vl) -> fun v -> v.Value = vl
+
+let f g z rq =
     let moveOp =
       rq.rawForm
       |> UTF8.toString
       |> Json.deserialize<MoveOp>
 
-    printfn "We have a MoveOp : %s" <| (match moveOp with
-        | ToVertex _a -> "To Vertex"
-        | AlongEdge _a -> "Along Edge"
-        | FirstEdge _a -> "Along First Matching Edge"
-        | FirstVertex _a -> "Along First Matching Vertex")
+    printfn "[[[[]]]] %s" (moveOp.ToString())
+    let moveRes = match moveOp with
+                    | ToVertex vval -> moveToVertex (g, !z, vval)
+                    | AlongEdge eege -> moveAlongEdge (g, !z, newEdge(newVert("dummy",-1),newVert("dummy",-2), eege))
+                    | FirstEdge s -> moveAlongFirstMatchingEdge(g, !z, (fun ve -> ve.Tag = s))
+                    | FirstVertex co -> moveAlongFirstMatchingVertex(g, !z, mkCompOp co)
+                    | Back -> Some(moveBack !z)
 
-    OK (Json.serialize moveOp)
+    match moveRes with
+        | None -> NOT_FOUND "damn"
+        | Some r ->
+            z := r
+            OK (Json.serialize (!z).Cursor)
 
-let app =
+let app g z =
     choose
-      [ GET >=> choose
-          [ path "/hello" >=> OK "Hello GET" ]
-        POST >=> choose
-          [ path "/move" >=> request f ] ] // curl -X POST -vv --data "{\"ToVertex\":\"camels\"}" http://localhost:8080/move
+      [ POST >=> choose
+          [ path "/move" >=> request (f g z) ] ]
 
+let addEdgeOrDie (g: AppGraph, e: TaggedEdge<Vert,string>) =
+    if g.AddEdge(e) then () else failwith "Failed to add edge"
+
+let addVertexOrDie (g: AppGraph, v: Vert) =
+    if g.AddVertex(v) then () else failwith "Failed to add edge"
+
+let generateFreshGraph: AppGraph =
+    let mutable g = new BidirectionalGraph<Vert, TaggedEdge<Vert,string>> ()
+    let vzero = newVert("zero", 0)
+    let vone = newVert("one", 1)
+    let vtwo = newVert("two", 2)
+    let vthree = newVert("three", 3)
+    let vfortytwo = newVert("forty-two", 42)
+
+    addVertexOrDie (g, vzero)
+    addVertexOrDie (g, vone)
+    addVertexOrDie (g, vtwo)
+    addVertexOrDie (g, vthree)
+    addVertexOrDie (g, vfortytwo)
+
+    addEdgeOrDie (g, (newEdge (vzero, vone, "add_one")))
+    addEdgeOrDie (g, (newEdge (vzero, vtwo, "add_two")))
+    addEdgeOrDie (g, (newEdge (vtwo, vthree, "two_three")))
+    addEdgeOrDie (g, (newEdge (vone, vthree, "one_three")))
+    addEdgeOrDie (g, (newEdge (vthree, vfortytwo, "end")))
+
+    g
+
+// curl -X POST -vvv --data '{"ToVertex":{"Tag":"one","Value":1}}' http://localhost:8080/move
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig app
+    let mutable g = generateFreshGraph
+    printfn "Graph Status verticies: %i, edges: %i" (g.VertexCount) (g.EdgeCount)
+    let root = newVert("zero", 0)
+    printfn "Root JSON: %s" (Json.serialize root)
+    let mutable freshZip = ref (createZipper root)
+    startWebServer defaultConfig (app g freshZip)
     0
